0dcf8da9b0d44e58ba6a942694a0639187b9b020
JavierRibaldelRio <javierribal@gmail.com>
2021-11-26 19:54:33 +0100

    He hecho una función que pasa el número del día de la semana a el nombre del día

---
+++ web/romun/src/PasarALetra.js
@@ -0,0 +1,10 @@
+//Esta función se ocupa de pasar a la letra correspondiente el número del día de las semana
+
+
+function pasarALetra(n) {
+
+    const letras = ['D', 'L', 'M', 'X', 'J', 'V', 'S'];
+
+    return letras[n];
+}
+export default pasarALetra;
