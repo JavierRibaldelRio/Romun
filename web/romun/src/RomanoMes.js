8dcf1a040bd8ffb14a7af057437e2703a95311e7
JavierRibaldelRio <javierribal@gmail.com>
2021-11-26 20:09:56 +0100

    He hecho una funcion que pasa de número decimal a romano

---
+++ web/romun/src/RomanoMes.js
@@ -0,0 +1,48 @@
+//Esta función se ocupa de pasar el més en número décimal a número romano (String)
+
+function pasarMesARomano(mes) {
+
+    var mesARomano;
+
+    switch (mes) {
+        case 1:
+            mesARomano = "I"
+            break;
+        case 2:
+            mesARomano = "II"
+            break;
+        case 3:
+            mesARomano = "III"
+            break;
+        case 4:
+            mesARomano = "IV"
+            break;
+        case 5:
+            mesARomano = "V"
+            break;
+        case 6:
+            mesARomano = "VI"
+            break;
+        case 7:
+            mesARomano = "VII"
+            break;
+        case 8:
+            mesARomano = "VIII"
+            break;
+        case 9:
+            mesARomano = "IX"
+            break;
+        case 10:
+            mesARomano = "X"
+            break;
+        case 11:
+            mesARomano = "XI"
+            break;
+        case 12:
+            mesARomano = "XII"
+            break;
+
+    }
+}
+
+export default pasarMesARomano;
