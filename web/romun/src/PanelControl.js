450f2bf78ef46eab43bfa805b3bf5640bc4589a4
JavierRibaldelRio <javierribal@gmail.com>
2021-11-27 21:20:06 +0100

    He editado el panel de control

--- web/romun/src/PanelControl.js
+++ web/romun/src/PanelControl.js
@@ -1,4 +1,5 @@
 import Boton from './Boton';
+import Fecha from './ClaseFecha';
 
 var React = require('react');
 
@@ -6,13 +7,61 @@ var React = require('react');
 
 class PanelControl extends React.Component {
 
+    constructor(props) {
+        super(props)
+
+        this.state = { objetoFecha: new Fecha() }
+    }
+
+
+    //Al ejecutar ejecuta la fucnion de autodate
+    fechaDeHoy() {
+
+        this.state.objetoFecha.autodate();
+
+        this.props.mandarTexto(this.state.objetoFecha.getFecha());  //Envia la fecha A  app
+
+
+
+    }
+
+
+    fechaDefinida(e) {
+
+        const fechaCalendario = this.state.fechaCalendario;
+        //Compreuba si hay algún dato dentro de fecha calendario
+        if (fechaCalendario === undefined) {
+            alert("Inserte algún valore en el selector de fechas")
+        }
+        else {
+
+            this.state.objetoFecha.fechaDefinida(fechaCalendario);
+
+            this.props.mandarTexto(this.state.objetoFecha.getFecha());  //Envia la fecha A  app
+
+        }
+
+    }
+
+    //Funcion que se ocupa de actualizar el valor de la fecha definida poreu usuario
+
+    actualizarEstadoCalendario(e) {
+
+        this.setState({ fechaCalendario: e.target.value });
+    }
 
     render() {
         return (
             <div className="Panel_de_Control">
 
+                <h1>ROMUN</h1>
 
-                <Boton textoBoton="Fecha de Hoy"></Boton>
+                <Boton funcionPasada={this.fechaDeHoy.bind(this)} textoBoton="Fecha de Hoy"></Boton>
+                <Boton type="submit" funcionPasada={this.fechaDefinida.bind(this)} textoBoton='Fecha insertada'></Boton>
+                <form>
+                    {/*Calendario  */}
+                    <input onChange={this.actualizarEstadoCalendario.bind(this)} id='calendario' name='calendario' type='date' step='1' ></input>
+                </form>
             </div>
         );
     }
