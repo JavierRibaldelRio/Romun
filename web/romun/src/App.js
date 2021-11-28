33e67cd79055e6963e0db08cca0892872c8f2094
JavierRibaldelRio <javierribal@gmail.com>
2021-11-28 01:01:12 +0100

    He avanzado con App.js y el index.js

--- web/romun/src/App.js
+++ web/romun/src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
-
-import Fecha from './Fecha';
 import PanelControl from './PanelControl';
+import ZonaFecha from './ZonaFecha';
+
 
 
 var React = require('react');   //Activa REACt
@@ -10,26 +9,22 @@ class App extends React.Component {
   constructor(props) {
 
     super(props);
+
+    this.state = { textoAMostrar: '' } //Almacene el texto que se va a mostrar
   }
 
-  funcion() {
-    var romano = new Fecha();
 
-    romano.autodate();
-
-    console.log(romano.getFecha());
-
+  actualizarTexto(texto) {
+    this.setState({ textoAMostrar: texto });
   }
 
   render() {
     return (
       <div className="App">
-        <button onClick={this.funcion.bind(this)}>fsad</button>
 
-        <PanelControl />
+        <PanelControl mandarTexto={this.actualizarTexto.bind(this)} />
 
-
-
+        <ZonaFecha texto={this.state.textoAMostrar}></ZonaFecha>
       </div>
     );
   }
